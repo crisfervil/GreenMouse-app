eccf56c43da0c9bcd805324dedbcbc0a20f4b188
Cristhian Fernandez <crisfervil@gmail.com>
2015-09-25 15:48:15 +0100

    Initial Commit

---
+++ build.js
@@ -0,0 +1,1 @@
+require('platypus/build/run');
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var platypusBuilder = require('platypus/gulp');
+
+platypusBuilder(gulp);
+
+// define your own tasks...
+
+/*gulp.task('build', function() {
+  return platypus.build(module.id);
+});
+
+gulp.task('start', function() {
+  return platypus.start();
+});*/
